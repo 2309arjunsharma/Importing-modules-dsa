75694a796856a97002f933dc05d36346ae8d59d1
Arjun Sharma <2309arjunsharma@gmail.com>
2024-12-30 10:49:07 +0530

    Importind modules and packages with python

---
+++ package/maths.py
@@ -0,0 +1,9 @@
+def problem(a,b,c,d,e,f):
+    return a+b-c*d/e%f
+
+def addition(a,b):
+    return a+b
+
+def subtraction(a,b):
+    return a-b
+
---
+++ test.py
@@ -0,0 +1,5 @@
+from package.maths import *
+from package.subpackages.math import *
+print(addition(4,6))
+print(subtraction(4,6))
+print(multiply(4,6))
